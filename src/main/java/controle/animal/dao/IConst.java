b497afd6ad0bc990df1c6fc397450b8cb9d1c01c
Júlia Dambrós <138934065+Juliadambros@users.noreply.github.com>
2024-12-03 22:36:29 -0300

    IConst.java

--- src/main/java/controle/animal/dao/IConst.java
+++ src/main/java/controle/animal/dao/IConst.java
@@ -2,6 +2,6 @@ package controle.animal.dao;
 
 public interface IConst {
     String url = "jdbc:postgresql://localhost:5432/HeldControl";
-    String usuario = "postgres";
-    String senha = "julia";
+    String usuario = "";
+    String senha = "";
 }
